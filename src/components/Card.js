3c1848b11d7ed2e7a7e932c2acb4df42a0febfb2
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-01-11 11:09:39 +0100

    Updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,16 @@
 import React from "react";
+
 import StarIcon from "../images/star.png"
 
 export default function Card(props) {
     return (
         <section className="card">
-            <img src={props.img} className="card--image" />
+            <img src={`/public/images/${props.img}`} className="card--image" />
             <div className="card--stats">
                 <img src={StarIcon} className="card--star" />
                 <span className="gray">{props.rating}</span>
                 <span className="gray">{(props.reviewCount)} • </span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price} </span> / person</p>
