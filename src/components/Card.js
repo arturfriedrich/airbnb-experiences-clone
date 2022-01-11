bc33943ea92e970c90bb121041b93fd5f1d574c1
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-01-11 16:36:05 +0100

    Updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,7 @@ import React from "react";
 import StarIcon from "./images/star.png"
 
 export default function Card(props) {
+    console.log(props)
     let badgeText
     if (props.openSpots === 0) {
         badgeText = "SOLD OUT"
@@ -13,11 +14,11 @@ export default function Card(props) {
     return (
         <section className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={require(`./images/${props.img}`)} className="card--image" />
+            <img src={require(`./images/${props.coverImg}`)} className="card--image" />
             <div className="card--stats">
                 <img src={StarIcon} className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
             </div>
             <p className="card--title">{props.title}</p>
